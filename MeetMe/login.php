d6ed10a958aeb695a77947b2e9b7b77b46a40789
matthewteh <76041610+matthewteh@users.noreply.github.com>
2021-10-27 22:08:43 +0800

    Changes in Login Page
    Style change and input style changes into mystyle.css

--- MeetMe/login.php
+++ MeetMe/login.php
@@ -44,43 +44,27 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Login</title>
+        <title>Login | Meetme v2</title>
+        <link rel="stylesheet" href="css/mystyle.css">
     </head>
     <body>
-        <style type = "text/css"> 
-            #text{
-                height: 25px;
-                border-radius: 5px;
-                padding: 4px;
-                border: solid thin #aaa;
-                width: 100%;
-            }
-
-            #button{
-                padding: 10px;
-                width: 100px;
-                color: white;
-                background-color: orange;
-                border: none;
-            }
-
-            #box{
-                background-color: skyblue;
-                margin: auto;
-                width: 300px;
-                padding: 20px;
-            }
-        </style>
-
-        <div id="box">
-            <form method="post">
-                <div style="font-size: 20px;margin: 10px;color:white">Login</div>
-                <input id="text" type="text" name="user_name" placeholder="username"><br><br>
-                <input id="text" type="password" name="password" placeholder="password"><br><br>
-
-                <input id="button" type="submit" value="Login"><br><br>
-                <a href="signup.php">Click to Sign Up</a><br><br>
-            </form>
+        <div class="logobar">
+            <img src="Image/MU Logo.png" class="logo"><a href="_self"></a>
         </div>
+            
+        <form method="post">
+        <div class="logincontainerbox">
+            <input class="loginboxtext" type="text" name="user_name" placeholder="username">
+            </br>
+            </br>
+            <input class="loginboxtext" type="password" name="password" placeholder="password">
+            </br>
+            <div class="logincontainerbutton">
+                <input class="loginpagebutton" type="reset" value="Clear">
+                <input class="loginpagebutton" type="submit" value="Login"><br><br>
+                <a href="signup.php">Click to Sign Up</a>
+            </div>
+        </div>
+        </form>
     </body>
 </html>
