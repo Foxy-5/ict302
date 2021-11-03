edf5d4d504f757d4405023f20f201d3cb6b752fb
tiff1127 <twotirt@gmail.com>
2021-11-03 19:40:27 +0800

    Update studentIdInput.php

--- MeetMe/studentIdInput.php
+++ MeetMe/studentIdInput.php
@@ -65,22 +65,55 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="UTF-8" />
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	    <link rel="stylesheet" href="css/mystyle.css">
 		<script>
 			//prevents website to send another POST request when user refreshes website
 			if ( window.history.replaceState ) {
 				window.history.replaceState( null, null, window.location.href );
 			}
 		</script>
+		<title>Booking Page | MeetMe v2</title>
 
 	</head>
 
 
 	<body>
-		<form method="post">
-			<label>StudentID : </label>
-			<input type="text" name="studentid">
-			<p id="errorMessage"></p>
-			<input type="submit" value="Book Meeting">
-		</form>
+		<nav class="navbar navbar-inverse">
+	        <div class="navbar-header">
+	            <a href="home.php"><img src="Image/MU Logo.png" height="80"></a>
+	        </div>
+	        <div class="navpaddingright">
+	            <ul class="nav navbar-nav">
+	                <li class="active"><a href="home.php">Home</a></li>
+	                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Appointment <span class="caret"></span></a>
+	                    <ul class="dropdown-menu">
+	                        <li class="item"><a href="uploadExcel.php">Upload Excel</a></li>
+	                        <li class="item"><a href="calendar.php">View Calendar</a></li>
+	                    </ul>
+	                </li>
+	                <li><a href="analytics.php">Analytics</a></li>
+	                <li><a href="about.php">About</a></li>
+	            </ul>
+	            <ul class="nav navbar-nav navbar-right">
+	                <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> My Profile</a></li>
+	                <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+	            </ul>
+	        </div>
+    	</nav>
+    	
+	    <div class="content">
+			<form method="post">
+				<label>StudentID : </label>
+				<input type="text" name="studentid">
+				<p id="errorMessage"></p>
+				<input type="submit" value="Book Meeting">
+			</form>
+		</div>
 	</body>
 </html>
