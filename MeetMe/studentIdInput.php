8519450cb942da8630cb15123a97687de7c41f94
tiff1127 <twotirt@gmail.com>
2021-11-13 04:15:39 +0800

    Updates to only accessible if there is auth key (dedicated link)

--- MeetMe/studentIdInput.php
+++ MeetMe/studentIdInput.php
@@ -1,53 +1,88 @@
 <?php
+	session_start();
 	//must be connected to the database before running the website
 	require_once("connection.php");
+	//var_dump($_SERVER);
+
+	if(isset($_GET['authkey'])){
+		$authkey = $_GET['authkey'];
+		//can use function from email.php
+		$authkeyquery = "SELECT StudentID FROM studentlist WHERE Auth_Key = '$authkey' limit 1;";
+		$authenticate = mysqli_query($con,$authkeyquery);
+
+		if($authenticate && mysqli_num_rows($authenticate) > 0){
+			$studentData = mysqli_fetch_assoc($authenticate);
+
+			$_SESSION['studentid'] = $studentData['StudentID'];
+
+			$query = "SELECT userid FROM list WHERE listid = (SELECT listid FROM studentlist WHERE Auth_key = '$authkey' limit 1)limit 1;";
+	        $result = mysqli_query($con,$query);
+	        if($result && mysqli_num_rows($result) > 0){
+	            $output = mysqli_fetch_assoc($result);
+	            $_SESSION['bookingstaffid'] = $output['userid'];
+	        }
+
+		}
+		else{
+			header("Location: error.php");
+			exit();
+		}
+	}
+	else{
+		header("Location: error.php");
+		exit();
+	}
 
 	//if the form is submitted(to search for student id)
 	if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
+		$errorMessage = false;
 
-		$studentid = $_POST['studentid'];
+		$studentidinput = $_POST['studentid'];
 
 		//if the input is not empty
-		if(!empty($studentid))
+		if(!empty($studentidinput))
 		{
 			//crafts query and queries from server database
-			$query = "select * from student where StudentID = '$studentid' limit 1";
+			/*$query = "select * from student where StudentID = '$studentid' limit 1";
 			$result = mysqli_query($con,$query);
 
 			//if the data is found
 			if($result && mysqli_num_rows($result) > 0)
 			{
 
-				$studentData = mysqli_fetch_assoc($result);
+				$studentData = mysqli_fetch_assoc($result);*/
 
-				if($studentData['StudentID'] == $studentid)
-				{
-					//saves the student id into current session and brings user to the booking page
-					$_SESSION['StudentID'] = $studentData['StudentID'];
-					header("Location: booking.php");
-					die;
-				}
+			if($studentidinput == $_SESSION['studentid']){
 
+				//saves the student id into current session and brings user to the booking page
+				$_SESSION['StudentID'] = $studentData['StudentID'];
+				header("Location: booking.php");
+				exit();
 			}
-
-			else
-			{
-
-				//show error message
-				echo <<<END
-						<script>
-							window.onload = function () {
-								document.getElementById('errorMessage').innerText = "Student id not found!";
-							};
-						</script>
-						END;
-
+			else{
+				$errorMessage = true;
 			}
 
 		}
+		else{
+			$errorMessage = true;
+			//show error message
+		}
 
-		else
+		if($errorMessage){
+			echo <<<END
+					<script>
+						window.onload = function () {
+							document.getElementById('errorMessage').innerText = "Student id not found!";
+						};
+					</script>
+					END;
+		}
+
+	}
+
+		/*else
 		{
 			
 			//show error message
@@ -58,42 +93,27 @@
 							};
 						</script>
 						END;
-		}
-	}
+		}*/
+		//header( "Location: {$_SERVER['REQUEST_URI']}", true, 303 );
+   		//exit();
+
+	
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8" />
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	    <link rel="stylesheet" href="css/mystyle.css">
 		<script>
 			//prevents website to send another POST request when user refreshes website
-			if ( window.history.replaceState ) {
+			/*if ( window.history.replaceState ) {
 				window.history.replaceState( null, null, window.location.href );
-			}
+			}*/
 		</script>
-		<title>Booking Page | MeetMe v2</title>
 
 	</head>
 
 
 	<body>
-		<nav class="navbar navbar-inverse">
-	        <div class="navbar-header">
-	            <a href="home.php"><img src="Image/MU Logo.png" height="80"></a>
-	        </div>
-	        <div class="navpaddingright">
-	            <ul class="nav navbar-nav">
-	                <li class="active"><a href="home.php">Home</a></li>
-	            </ul>
-	        </div>
-    	</nav>
     	
 	    <div class="content">
 			<form method="post">
