e4637212be4bc563b8c15215d343d83b5b28f108
Foxy-5 <83771600+Foxy-5@users.noreply.github.com>
2021-11-19 01:02:36 +0800

    fixed query

--- MeetMe/viewbooking.php
+++ MeetMe/viewbooking.php
@@ -8,7 +8,7 @@ include("include/function.php");
 $user_data = check_login($con);
 $bookingId = $_GET['bookingid'];
 // $query1 = "Select Booking_date, Booking_start, Booking_end, student.First_name, student.Last_name, BookingID, Comment, PreviousMeetingID , booking.StudentID, Duration, Status from booking, student where booking.bookingID = '$bookingid' and booking.StudentID = student.StudentID LIMIT 1";
-$query1 = "Select Booking_date, Booking_start, Booking_end, case WHEN booking.StudentID is NULL THEN NULL ELSE student.First_name end as First_name, case WHEN booking.StudentID is NULL THEN NULL ELSE student.Last_name end as Last_name, BookingID, Comment, PreviousMeetingID , booking.StudentID, Duration, Status, booking.Initial from booking, student where booking.Auth_key = '$bookingId' LIMIT 1";
+$query1 = "Select Booking_date, Booking_start, Booking_end, case WHEN booking.StudentID is NULL THEN NULL ELSE student.First_name end as First_name, case WHEN booking.StudentID is NULL THEN NULL ELSE student.Last_name end as Last_name, BookingID, Comment, PreviousMeetingID , booking.StudentID, Duration, Status, booking.Initial from booking LEFT JOIN student ON booking.StudentID = student.StudentID where booking.Auth_key = '$bookingId'limit 1";
 $result1 = mysqli_query($con, $query1);
 if(!mysqli_num_rows($result1)>0){
     echo "<script>alert('Invalid meeting')</script>";
